e76e4dbb73790fadc730c99d26d278b6d6387b56
Henrik Élő <henrikeloe@gmail.com>
2024-02-17 16:47:44 +0100

    First Commit

---
+++ Assets/Code/Scripts/GridManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManager : MonoBehaviour
+{
+
+    [SerializeField] Tile[] tiles;
+    [SerializeField] int gridHeight = 20;
+    [SerializeField] int gridWidth = 20;
+    [SerializeField] float tileSize = 0.5f;
+    [SerializeField] float chanceToSwitchTile = 0.3f;
+    [SerializeField] Tile previousTilePlaced = null;
+
+    Dictionary<Vector3Int, Tile> tileMap = new Dictionary<Vector3Int, Tile>();
+    int offsetX = 10;
+    int offsetZ = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetX = gridHeight / 4;
+        offsetZ = gridWidth / 4;
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {   
+        
+        for (int x  = 0; x < gridHeight; x++) {
+            for (int z = 0; z < gridWidth; z++) {
+                Tile tileToPlace;
+                if (previousTilePlaced) {
+                    tileToPlace = UnityEngine.Random.Range(0.0f, 1.0f) <= chanceToSwitchTile ? tiles[UnityEngine.Random.Range(0, tiles.Length)]: previousTilePlaced;
+                }  else {
+                    tileToPlace = tiles[UnityEngine.Random.Range(0, tiles.Length)];
+                }
+                previousTilePlaced = tileToPlace;
+                Tile newTile = Instantiate(tileToPlace, transform);
+                float xCoordinate = x * tileSize - offsetX;
+                float zCoordinate = z * tileSize - offsetZ;
+                Vector3 newPosition = new Vector3(xCoordinate, 0.0f, zCoordinate);
+
+                newTile.transform.position = newPosition;
+                tileMap[TransformVector3PositionToInt(newPosition)] = newTile;
+                newTile.name = $"{xCoordinate}, {zCoordinate}";
+            }
+        }
+
+        foreach (var tile in tileMap.Keys) {
+            Debug.Log(tile, tileMap[tile]);
+        }
+    }
+
+    Vector3Int TransformVector3PositionToInt(Vector3 position) {
+        return new Vector3Int((int)(position.x * 2), (int)(position.y * 2), (int)(position.z * 2));
+    }
+
+    public Tile GetTileAtPosition(Vector3 position) {
+        Debug.Log(position);
+        return tileMap[TransformVector3PositionToInt(position)];
+    }
+}
---
+++ Assets/Code/Scripts/Highlight.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+
+    [SerializeField] Material highlightMaterial;
+    [SerializeField] Material originalMaterial;
+    
+    private void Awake() {
+        originalMaterial = gameObject.GetComponent<MeshRenderer>()?.material;
+    }
+
+    public void ToggleHighlight(bool on) {
+        gameObject.GetComponent<MeshRenderer>().material = on? highlightMaterial : originalMaterial;
+    }
+}
