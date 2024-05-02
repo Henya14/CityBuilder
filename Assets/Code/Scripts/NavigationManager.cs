85449054dee9ec691c634de396cd3c905e6d2bfd
Henrik Élő <henrikeloe@gmail.com>
2024-05-02 10:57:22 +0200

    Add navigation manager to main, change...
    adjacencymatrix behaviour so it uses selectable objects instead of AbstractBuildingType

--- Assets/Code/Scripts/NavigationManager.cs
+++ Assets/Code/Scripts/NavigationManager.cs
@@ -2,24 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
-struct NeighbourWeights { public int WeightFromNeighbour; public int WeightToNeighbour; };
+public struct NeighbourWeights { public int WeightFromNeighbour; public int WeightToNeighbour; };
 
 public class BuildingAdjacencyMatrix
 {
     List<List<int>> weights = new List<List<int>>();
-    List<AbstractBuildingType> buildigs = new List<AbstractBuildingType>();
+    List<SelectableObject> buildigs = new List<SelectableObject>();
 
 
-    int GetWeightOfPath(AbstractBuildingType from, AbstractBuildingType to)
+    public int GetWeightOfPath(SelectableObject from, SelectableObject to)
     {
         int indexOfFromBuilding = buildigs.IndexOf(from);
         int indexOfToBuilding = buildigs.IndexOf(to);
         return weights[indexOfFromBuilding][indexOfToBuilding];
     }
 
-    void AddBuilding(AbstractBuildingType building, Dictionary<AbstractBuildingType, NeighbourWeights> neighbours)
+    public void AddBuilding(SelectableObject building, Dictionary<SelectableObject, NeighbourWeights> neighbours)
     {
         for (int i = 0; i < weights.Count; i++)
         {
@@ -27,7 +28,8 @@ public class BuildingAdjacencyMatrix
         }
 
         int widthOfMatrix = weights[0].Count;
-        if (widthOfMatrix == 0) {
+        if (widthOfMatrix == 0)
+        {
             widthOfMatrix = 1;
         }
 
@@ -38,15 +40,16 @@ public class BuildingAdjacencyMatrix
         {
             int indexOfNeighbour = buildigs.IndexOf(neighbour.Key);
             int indexOfBuilding = buildigs.IndexOf(building);
-            if(indexOfNeighbour == -1) {
-                throw new Exception($"Neighbour not in the list of buildings for {building.name} {building.gridPositions}");
-            } 
+            if (indexOfNeighbour == -1)
+            {
+                throw new Exception($"Neighbour not in the list of buildings for {building.GetGameObject().name} {building.Description()}");
+            }
             weights[indexOfNeighbour][indexOfBuilding] = neighbour.Value.WeightFromNeighbour;
             weights[indexOfBuilding][indexOfNeighbour] = neighbour.Value.WeightToNeighbour;
         }
     }
 
-    void RemoveBuilding(AbstractBuildingType building)
+    public void RemoveBuilding(SelectableObject building)
     {
         int indexOfBuilding = buildigs.IndexOf(building);
         for (int i = 0; i < weights.Count; i++)
@@ -58,10 +61,25 @@ public class BuildingAdjacencyMatrix
         buildigs.Remove(building);
     }
 
+    public int GetIndexOfBuildingAtGridPosition(Vector3Int gridPosition)
+    {
+        int indexOfBuilding = buildigs.FindIndex(b => b.GetGridPosition() == gridPosition);
+        return indexOfBuilding;
+    }
+
+    public SelectableObject GetBuildingAtGridPosition(Vector3Int gridPosition)
+    {
+        SelectableObject building = buildigs.Find(b => b.GetGridPosition() == gridPosition);
+        return building;
+    }
+
+
+
 }
 public class NavigationManager : MonoBehaviour
 {
     BuildingAdjacencyMatrix adjacencyMatrix = new BuildingAdjacencyMatrix();
+    List<SelectableObject> selectedObjects = new List<SelectableObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -75,5 +93,29 @@ public class NavigationManager : MonoBehaviour
 
     }
 
+    public void ObjectSelected(SelectableObject selectedObject)
+    {
+        if (selectedObjects.Count == 2)
+        {
+            DeselectObjects();
+        }
+        if (adjacencyMatrix.GetBuildingAtGridPosition(selectedObject.GetGridPosition()) != null) {
+            selectedObjects.Add(selectedObject);
+            selectedObject.FreezeHighlight(true);
+            selectedObject.ToggleHighlight(true);
+
+        }
+    }
+
+    public void DeselectObjects()
+    {
+        selectedObjects.ForEach(so =>
+        {
+            so.FreezeHighlight(false);
+            so.ToggleHighlight(false);
+        });
+
+        selectedObjects.Clear();
+    }
 
 }
