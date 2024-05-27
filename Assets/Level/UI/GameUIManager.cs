903a88f26637f093dfdbb40aa5dba44d26addf35
Patrik <vendelp2000@gmail.com>
2024-05-27 12:34:17 +0200

    taxes menu added

--- Assets/Level/UI/GameUIManager.cs
+++ Assets/Level/UI/GameUIManager.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public enum GameMode {
+public enum GameMode
+{
     SelectionMode,
     BuildMode,
     NavigationMode
@@ -36,8 +38,12 @@ public class GameUIManager : MonoBehaviour
     Button buildingsButton;
     VisualElement buildingHud;
 
+    Button taxesButton;
+    VisualElement taxesHud;
+    List<Label> labels = new List<Label>();
+
     List<Button> gameModeSelectorButtons = new List<Button>();
-    public GameMode selectedGameMode {get; set;} = GameMode.SelectionMode;
+    public GameMode selectedGameMode { get; set; } = GameMode.SelectionMode;
 
     private BuildModeManager buildModeManager;
     private GridManager gridManager;
@@ -51,7 +57,8 @@ public class GameUIManager : MonoBehaviour
     private float timeWhenDisappear;
 
     const string SELECTED_BUTTON_CLASS_NAME = "selected";
-    private void OnEnable() {
+    private void OnEnable()
+    {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         selectionModeButton = root.Q<Button>("selection-mode-button");
         buildModeButton = root.Q<Button>("building-mode-button");
@@ -59,25 +66,32 @@ public class GameUIManager : MonoBehaviour
         infoContainer = root.Q<VisualElement>("info-text-container");
         infoContainerText = root.Q<Label>("info-text-label");
         buildingList = root.Q<ListView>("building-list");
-
         balanceLabel = root.Q<Label>("balance-text");
         electricityLabel = root.Q<Label>("electricity-text");
         coalLabel = root.Q<Label>("coal-text");
         woodLabel = root.Q<Label>("wood-text");
-
         timeStartStopButton = root.Q<Button>("time-start-stop-button");
         timeForwardButton = root.Q<Button>("time-forward-button");
         timeTextField = root.Q<Label>("time-text-field");
-
         moralityViewToggle = root.Q<Toggle>("morality-toggle");
-
         buildingsButton = root.Q<Button>("buildings-button");
         buildingHud = root.Q<VisualElement>("building-hud-container");
-
         questText = root.Q<Label>("quest-text");
+        taxesButton = root.Q<Button>("taxes-button");
+        taxesHud = root.Q<VisualElement>("tax-box-container");
 
+        var taxButtons = root.Query<Button>(null, "tax-button");
+        taxButtons.ForEach(SetupButton);
+
+
+
+        var taxLabels = root.Query<Label>(null, "tax-label");
+        //taxLabels.ForEach(SetupLabel);
+
+        taxLabels.ForEach(labels.Add);
     }
-    void Start() {
+    void Start()
+    {
         selectionModeButton.clicked += OnSelectionModeButtonClicked;
         selectionModeButton.AddToClassList(SELECTED_BUTTON_CLASS_NAME);
         buildModeButton.clicked += OnBuildModeButtonClicked;
@@ -112,6 +126,9 @@ public class GameUIManager : MonoBehaviour
         buildingsButton.clicked += ChangeVisibleOnBuildingHud;
 
         PlayerBalance.OnPlayerStatsChanged += UpdateBalanceText;
+        taxesButton.clicked += TaxesMenu;
+
+        UpdateTaxes();
     }
 
     private void Update()
@@ -127,7 +144,7 @@ public class GameUIManager : MonoBehaviour
         HideSpecialBuildings();
         selectedGameMode = GameMode.SelectionMode;
         ResetOnGameModeChange();
-        selectionModeButton.AddToClassList(SELECTED_BUTTON_CLASS_NAME); 
+        selectionModeButton.AddToClassList(SELECTED_BUTTON_CLASS_NAME);
     }
 
     private void ResetOnGameModeChange()
@@ -139,66 +156,82 @@ public class GameUIManager : MonoBehaviour
         buildingList.style.display = DisplayStyle.None;
     }
 
-    void OnBuildModeButtonClicked() {
+    void OnBuildModeButtonClicked()
+    {
         HideSpecialBuildings();
         SetSelectedForGameModeSelectorButtons(false);
         selectedGameMode = GameMode.BuildMode;
         buildModeButton.AddToClassList(SELECTED_BUTTON_CLASS_NAME);
         buildingList.style.display = DisplayStyle.Flex;
-        InitializeBuildingsList();      
+        InitializeBuildingsList();
     }
 
-    void OnNavigationModeButtonClicked() {
+    void OnNavigationModeButtonClicked()
+    {
         selectedGameMode = GameMode.NavigationMode;
         ResetOnGameModeChange();
         navigationModeButton.AddToClassList(SELECTED_BUTTON_CLASS_NAME);
     }
-    void InitializeBuildingsList() {
+    void InitializeBuildingsList()
+    {
         var buildings = buildModeManager.GetBuildingDatas();
 
-        buildingList.makeItem = () => {
-                var newListEntry = buildingListElementTemplate.Instantiate();
-                var newListEntryLogic = new BuildingListEntryController();
-                newListEntry.userData = newListEntryLogic;
+        buildingList.makeItem = () =>
+        {
+            var newListEntry = buildingListElementTemplate.Instantiate();
+            var newListEntryLogic = new BuildingListEntryController();
+            newListEntry.userData = newListEntryLogic;
 
-                newListEntryLogic.SetVisualElement(newListEntry);
-                return newListEntry;
-            };
+            newListEntryLogic.SetVisualElement(newListEntry);
+            return newListEntry;
+        };
 
-        buildingList.bindItem = (item, index) => {
+        buildingList.bindItem = (item, index) =>
+        {
             (item.userData as BuildingListEntryController).SetBuildingData(buildings[index]);
         };
         buildingList.fixedItemHeight = 45;
         buildingList.itemsSource = buildings;
 
-        buildingList.selectionChanged += OnBuildingSelected; 
+        buildingList.selectionChanged += OnBuildingSelected;
         buildingList.SetSelection(0);
     }
 
-    void OnBuildingSelected(IEnumerable<object> selectedItems) {
+    void OnBuildingSelected(IEnumerable<object> selectedItems)
+    {
         var selectedBuilding = buildingList.selectedItem as BuildingData;
-        if (selectedBuilding == null) {
-            gridManager.ChangeSelection(new Vector2Int(1,1), null, null);
+        if (selectedBuilding == null)
+        {
+            gridManager.ChangeSelection(new Vector2Int(1, 1), null, null);
             buildModeManager.BuildingDataSelected(null);
-        } else {
+        }
+        else
+        {
             buildModeManager.BuildingDataSelected(selectedBuilding);
             gridManager.ChangeSelection(selectedBuilding.size, selectedBuilding.buildingType, selectedBuilding.prefab);
         }
     }
 
-    void SetSelectedForGameModeSelectorButtons(bool selected) {
-        foreach (Button button in  gameModeSelectorButtons) {
-            if (selected) {
+    void SetSelectedForGameModeSelectorButtons(bool selected)
+    {
+        foreach (Button button in gameModeSelectorButtons)
+        {
+            if (selected)
+            {
                 button.AddToClassList(SELECTED_BUTTON_CLASS_NAME);
-            } else {
+            }
+            else
+            {
                 button.RemoveFromClassList(SELECTED_BUTTON_CLASS_NAME);
             }
         }
     }
 
-    public void ObjectSelected(SelectableObject selectedObject, Dictionary<Vector3, List<Vector3Int>> placingPositionsWithGridPositions) {
-        switch (selectedGameMode) {
-            case GameMode.SelectionMode: 
+    public void ObjectSelected(SelectableObject selectedObject, Dictionary<Vector3, List<Vector3Int>> placingPositionsWithGridPositions)
+    {
+        switch (selectedGameMode)
+        {
+            case GameMode.SelectionMode:
                 ObjectSelectedInSelectionMode(selectedObject);
                 break;
             case GameMode.BuildMode:
@@ -210,53 +243,68 @@ public class GameUIManager : MonoBehaviour
             default:
                 break;
         }
-       
+
     }
 
-    void ObjectSelectedInSelectionMode(SelectableObject selectedObject) {
-        if (selectedObject != null) {
-            var displayText = selectedObject.GetDescription() ;
+    void ObjectSelectedInSelectionMode(SelectableObject selectedObject)
+    {
+        if (selectedObject != null)
+        {
+            var displayText = selectedObject.GetDescription();
             infoContainer.style.display = DisplayStyle.Flex;
             var tile = selectedObject.GetGameObject().GetComponent<Tile>();
-            if (tile != null) {
+            if (tile != null)
+            {
                 displayText += " morality:  " + tile.tileMorality.moralityLevel;
             }
             infoContainerText.text = displayText;
-        
-        } else {
+
+        }
+        else
+        {
             infoContainer.style.display = DisplayStyle.None;
             infoContainerText.text = "";
         }
     }
 
-    void ObjectSelectedInBuildMode(SelectableObject selectedObject, Dictionary<Vector3, List<Vector3Int>> placingPositionsWithGridPositions) {
-         if (selectedObject != null) {
+    void ObjectSelectedInBuildMode(SelectableObject selectedObject, Dictionary<Vector3, List<Vector3Int>> placingPositionsWithGridPositions)
+    {
+        if (selectedObject != null)
+        {
             buildModeManager.ObjectSelected(placingPositionsWithGridPositions);
         }
     }
 
-    void ObjectSelectedInNavigationMode(SelectableObject selectedObject) {
-         if (selectedObject != null) {
+    void ObjectSelectedInNavigationMode(SelectableObject selectedObject)
+    {
+        if (selectedObject != null)
+        {
             navigationManager.ObjectSelected(selectedObject);
         }
     }
 
-    void OnTimeStartStopButtonClicked() {
+    void OnTimeStartStopButtonClicked()
+    {
         TimeManager.instance.StartStopTimer();
     }
 
-    void OnTimeForwardButtonClicked() {
+    void OnTimeForwardButtonClicked()
+    {
         TimeManager.instance.ChangeTimerSpeed();
     }
 
-    void UpdateTimer() {
+    void UpdateTimer()
+    {
         timeTextField.text = $"{TimeManager.Hour:00}:{TimeManager.Minute:00}";
     }
 
-    void LoadBuildings() {
+    void LoadBuildings()
+    {
         BuildingData[] loadedObjects = Resources.LoadAll<BuildingData>("Buildings");
-        foreach (BuildingData obj in loadedObjects) {
-            if (obj.BuyableBuilding) {
+        foreach (BuildingData obj in loadedObjects)
+        {
+            if (obj.BuyableBuilding)
+            {
                 Button tempbutton = new Button();
                 tempbutton.text = obj.Name;
                 tempbutton.clicked += () => buildModeManager.BuildingDataSelected(obj);
@@ -264,7 +312,7 @@ public class GameUIManager : MonoBehaviour
                 buildingHud.Add(tempbutton);
             }
         }
-        
+
     }
 
     void HideSpecialBuildings()
@@ -273,13 +321,16 @@ public class GameUIManager : MonoBehaviour
             buildingHud.visible = !buildingHud.visible;
     }
 
-    void ChangeVisibleOnBuildingHud() {
-        buildingHud.visible = !buildingHud.visible; 
-        if(buildingHud.visible) {
+    void ChangeVisibleOnBuildingHud()
+    {
+        buildingHud.visible = !buildingHud.visible;
+        if (buildingHud.visible)
+        {
             ResetOnGameModeChange();
             selectedGameMode = GameMode.BuildMode;
         }
-        else {
+        else
+        {
             OnSelectionModeButtonClicked();
         }
     }
@@ -298,4 +349,53 @@ public class GameUIManager : MonoBehaviour
         questText.text = text;
         timeWhenDisappear = Time.time + timeToAppear;
     }
+
+    void TaxesMenu()
+    {
+        taxesHud.visible = !taxesHud.visible;
+    }
+
+    private void SetupButton(Button button)
+    {
+        //Subscribe to the clickedWithEventInfo event
+        button.clickable.clickedWithEventInfo += Clickable_clickedWithEventInfo;
+    }
+
+    private void Clickable_clickedWithEventInfo(EventBase obj)
+    {
+        var button = (Button)obj.target;
+        List<string> buttonFunction = button.name.Split("-").ToList();
+        if (buttonFunction[2] == "plus")
+        {
+            PlayerBalance.instance.IncreaseTaxes(buttonFunction[1], int.Parse(buttonFunction[0]) - 1);
+        }
+        if (buttonFunction[2] == "minus")
+        {
+            PlayerBalance.instance.DecreaseTaxes(buttonFunction[1], int.Parse(buttonFunction[0]) - 1);
+        }
+        UpdateTaxes();
+    }
+    private void UpdateTaxes()
+    {
+        for (int i = 0; i < labels.Count; i++)
+        {
+            string selectedValue = "";
+            List<string> labelName = labels[i].name.Split("-").ToList();
+            if (labelName[1] == "resident")
+            {
+                selectedValue = PlayerBalance.instance.ResidentsTaxes[int.Parse(labelName[0]) - 1].ToString();
+            }
+            else if (labelName[1] == "shop")
+            {
+                selectedValue = PlayerBalance.instance.ShopTaxes[int.Parse(labelName[0]) - 1].ToString();
+            }
+            else if (labelName[1] == "factory")
+            {
+                selectedValue = PlayerBalance.instance.FactoryTaxes[int.Parse(labelName[0]) - 1].ToString();
+            }
+            labels[i].text = selectedValue + " %";
+        }
+
+    }
+
 }
