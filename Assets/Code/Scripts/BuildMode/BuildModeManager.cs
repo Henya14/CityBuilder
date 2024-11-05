e34f85e9009b0534916915b10a6195e215a2c0eb
Henrik Élő <henrikeloe@gmail.com>
2024-11-05 11:13:50 +0100

    Remove import

--- Assets/Code/Scripts/BuildMode/BuildModeManager.cs
+++ Assets/Code/Scripts/BuildMode/BuildModeManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Xml.Schema;
-using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.PackageManager;
